e6070a35ccb46c1a404968e2da90b3449435651b
Todd Wolfson <todd@twolfson.com>
2014-11-04 01:07:07 -0800

    Fixing up loops

--- test/expected-files/basic-loops.js
+++ test/expected-files/basic-loops.js
@@ -2,18 +2,6 @@ for (var i = 0, j = 1; i < 10; i++ ) {
   // Code goes here
 }
 
-// TODO: Not sure if this would actually work...
-// TODO: If not, move to single var
-for (var i = 0, j in obj) {
+for (var i in obj) {
   // Code goes here
 }
-
-// TODO: Not sure if this would actually work...
-while (var a = 0, b = 1) {
-  // Code goes here
-}
-
-// TODO: Not sure if this would actually work...
-do {
-  // Code goes here
-} while (var u = 0, v = 1);
