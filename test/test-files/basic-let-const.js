dd0d2d10632310b0a1d2fec0fbc6006b3ca5630b
Todd Wolfson <todd@twolfson.com>
2014-11-04 01:07:16 -0800

    Added test case for `let` and `const`

---
+++ test/test-files/basic-let-const.js
@@ -0,0 +1,11 @@
+// TODO: Are we allowed to define multiple consts via a comma?
+const a = 'hello',
+      b = 'world';
+
+for (let i = 0, j = 1; i < 10; i++ ) {
+  // Code goes here
+}
+
+for (let i in obj) {
+  // Code goes here
+}
