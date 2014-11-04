899de0d839cfdc7508c4f5d6a83011bc1bcce765
Todd Wolfson <todd@twolfson.com>
2014-11-04 01:43:24 -0800

    Test is green =D

--- test/expected-files/advanced-multi-line-semicolon-less.js
+++ test/expected-files/advanced-multi-line-semicolon-less.js
@@ -1,7 +1,7 @@
 // TODO: This shouldn't be necessary. Stop doing sniffing on `;`...
 // TODO: Also, add a test for indented with semicolon. You will see they are all the same case
 var a = function () {
-      // moar code
-    },
-    b = 'look ma\
+    // moar code
+  }
+var b = 'look ma\
     no semicolon'
