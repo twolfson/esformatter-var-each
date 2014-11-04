24081885a3d876fcf2fb8ff28571dee4fe84e2bb
Todd Wolfson <todd@twolfson.com>
2014-11-03 21:32:28 -0800

    Copied/updated test from esformatter-phonetic

--- test/esformatter-var-each-test.js
+++ test/esformatter-var-each-test.js
@@ -1,11 +1,35 @@
-var esformatter_var_each = require('../');
+// Load in dependencies
+var assert = require('assert');
+var fs = require('fs');
+var esformatter = require('esformatter');
+var esformatterVarEach = require('../');
 
+// Register our plugin
+esformatter.register(esformatterVarEach);
+
+// Define test utilities
+var testUtils = {
+  format: function (filepath) {
+    before(function formatFn () {
+      // Format our content
+      var input = fs.readFileSync(filepath, 'utf8');
+      this.output = esformatter.format(input);
+    });
+    after(function cleanup () {
+      // Cleanup output
+      delete this.output;
+    });
+  }
+};
+
+// Start our tests
 describe('esformatter-var-each', function () {
-  before(function () {
+  describe('formatting a JS file with comma-first variables', function () {
+    testUtils.format(__dirname + '/test-files/comma-first.js');
 
-  });
-
-  it('', function () {
-
+    it('converts each variable to its own `var` statement', function () {
+      var expectedOutput = fs.readFileSync(__dirname + '/expected-files/comma-first.js', 'utf8');
+      assert.strictEqual(this.output, expectedOutput);
+    });
   });
 });
